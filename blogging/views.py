e170713ebc2185fbf12156b8ff6a6e42fc17dd03
Salvador <sgavon@uw.edu>
2022-09-17 10:49:24 -0700

    Assignment 09 Start

--- blogging/views.py
+++ blogging/views.py
@@ -8,7 +8,7 @@ from django.views.generic.detail import DetailView
 
 
 class BlogListView(ListView):
-    queryset = Post.objects.order_by('published_date').exclude(published_date__exact=None)
+    queryset = Post.objects.order_by('-published_date').exclude(published_date__exact=None)
     template_name = 'blogging/list.html'
 
 
